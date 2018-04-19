d7fb69c9ff249c9ae889ba1f5fe8d5688da5d062
atischenkoCV <atischenko.public@gmail.com>
2018-04-19 18:06:51 +0300

    [c++/collections] - implement linked list

---
+++ cpp/collections/CollectionsLibrary/src/LinkedList.cpp
@@ -0,0 +1,110 @@
+/*
+ * LinkedList.cpp
+ *
+ *  Created on: Apr 18, 2018
+ *      Author: atischenko
+ */
+
+#include "LinkedList.h"
+
+using ticher777::LinkedList;
+
+LinkedList::LinkedList() {
+	head = tail = nullptr;
+	listSize = 0;
+}
+;
+
+LinkedList::~LinkedList() {
+	int currentSize;
+	while ((currentSize = size()) != 0) {
+		doRemove(currentSize - 1);
+	}
+}
+;
+
+LinkedList::LinkedNode* LinkedList::doGetNode(int index) const {
+	int currentIndex = 0;
+	LinkedNode *currentNode = head;
+	while (currentIndex != index) {
+		currentIndex++;
+		currentNode = currentNode->next;
+	}
+	return currentNode;
+}
+;
+
+void ticher777::LinkedList::doAdd(int index, int value) {
+	LinkedNode *newNode = new LinkedNode;
+	newNode->value = value;
+
+	if (index == 0) {
+		newNode->prev = nullptr;
+		if (head == nullptr) {
+			head = tail = newNode;
+			linkNodes(newNode, nullptr);
+		}
+		else {
+			linkNodes(newNode, head);
+			head = newNode;
+		}
+	}
+	else if (index == size()) {
+		linkNodes(newNode, nullptr);
+		if (tail == nullptr) {
+			head = tail = newNode;
+			linkNodes(nullptr, newNode);
+		}
+		else {
+			linkNodes(tail, newNode);
+			tail = newNode;
+		}
+	}
+	else {
+		LinkedNode *currentNode = doGetNode(index);
+		linkNodes(currentNode->prev, newNode);
+		linkNodes(newNode, currentNode);
+	}
+
+	listSize++;
+}
+;
+
+int LinkedList::doRemove(int index) {
+	int result;
+	if (index == 0) {
+		result = head->value;
+		if (head == tail) {
+			delete head;
+			head = tail = nullptr;
+		}
+		else {
+			LinkedNode *newHead = head->next;
+			linkNodes(head->prev, head->next);
+			delete head;
+			head = newHead;
+		}
+	}
+	else if (index == size() - 1) {
+		result = tail->value;
+
+		LinkedNode *newTail = tail->prev;
+		linkNodes(tail->prev, tail->next);
+
+		delete tail;
+
+		tail = newTail;
+	}
+	else {
+		LinkedNode *currentNode = doGetNode(index);
+		result = currentNode->value;
+		linkNodes(currentNode->prev, currentNode->next);
+
+		delete currentNode;
+	}
+
+	listSize--;
+
+	return result;
+};
+
---
+++ cpp/collections/CollectionsLibrary/src/LinkedList.h
@@ -0,0 +1,59 @@
+/*
+ * LinkedList.h
+ *
+ *  Created on: Apr 18, 2018
+ *      Author: atischenko
+ */
+
+#ifndef LINKEDLIST_H_
+#define LINKEDLIST_H_
+
+#include "List.h"
+
+namespace ticher777 {
+
+	class LinkedList: public List {
+		public:
+			LinkedList();
+
+			virtual ~LinkedList();
+
+			virtual int size() const override {
+				return listSize;
+			}
+		protected:
+			struct LinkedNode {
+					int value;
+					LinkedNode *next;
+					LinkedNode *prev;
+			};
+
+			int doGet(int index) const override {
+				return doGetNode(index)->value;
+			}
+
+			void doAdd(int index, int value) override;
+
+			int doRemove(int index) override;
+
+			LinkedNode* doGetNode(int index) const;
+
+			void linkNodes(LinkedNode* prev, LinkedNode* next) const {
+				if (prev != nullptr) {
+					prev->next = next;
+				}
+				if (next != nullptr) {
+					next->prev = prev;
+				}
+			}
+			;
+		private:
+
+			LinkedNode *head;
+			LinkedNode *tail;
+			int listSize;
+	};
+
+} /* namespace ticher777 */
+
+#endif /* LINKEDLIST_H_ */
