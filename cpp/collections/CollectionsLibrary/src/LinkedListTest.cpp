b1b4e4b79ce6fdfe85b3c83dea7bb1ddc053b600
atischenkoCV <atischenko.public@gmail.com>
2018-04-22 19:37:58 +0300

    [c++/collections] - add tests for 'remove' method

--- cpp/collections/CollectionsLibrary/src/LinkedListTest.cpp
+++ cpp/collections/CollectionsLibrary/src/LinkedListTest.cpp
@@ -30,11 +30,11 @@ TEST_CASE( "LinkedList#add" , "[LinkedList]") {
 
 	REQUIRE_THROWS_WITH(list.add(-1), "List is full-filled.");
 }
-	SECTION( "Add out of range" ) {
-			REQUIRE_THROWS_WITH(list.add(10, -1), "Index 10 is out of range for 'add' operation. Valid range: [0, 0]");
-			list.add(11);
-			REQUIRE_THROWS_WITH(list.add(-1, 20), "Index -1 is out of range for 'add' operation. Valid range: [0, 1]");
-	}
+	SECTION( "Add out of range" ){
+	REQUIRE_THROWS_WITH(list.add(10, -1), "Index 10 is out of range for 'add' operation. Valid range: [0, 0]");
+	list.add(11);
+	REQUIRE_THROWS_WITH(list.add(-1, 20), "Index -1 is out of range for 'add' operation. Valid range: [0, 1]");
+}
 
 	SECTION( "Add random values"){
 	srand (time(nullptr));
@@ -62,14 +62,67 @@ TEST_CASE( "LinkedList#get" , "[LinkedList]") {
 	}
 
 }
-	SECTION( "Get bound indexes") {
-		int listSize = 200;
-		for (int i = 0; i < listSize; i++) {
-				list.add(i);
-			}
-		list.get(0);
-		list.get(listSize - 1);
-		REQUIRE_THROWS_WITH(list.get(-1), "Index -1 is out of range for 'get' operation. Valid range: [0, 199]");
+	SECTION( "Get bound indexes"){
+	int listSize = 200;
+	for (int i = 0; i < listSize; i++) {
+		list.add(i);
 	}
+	list.get(0);
+	list.get(listSize - 1);
+	REQUIRE_THROWS_WITH(list.get(-1), "Index -1 is out of range for 'get' operation. Valid range: [0, 199]");
+}
+}
+
+TEST_CASE( "LinkedList#remove" , "[LinkedList]" ) {
+	ticher777::LinkedList list;
+	REQUIRE(list.size() == 0);
+
+	SECTION( "Check remove the last element in the list" ){
+	int listSize = 200;
+
+	for (int i = 0; i < listSize; i++) {
+		list.add(i);
+	}
+	for (int i = 0; i < listSize; i++) {
+		list.remove(list.size() - 1);
+	}
+
+	REQUIRE(list.size() == 0);
+
+}
+	SECTION( "Check remove the first element in the list" ){
+	int listSize = 200;
+
+	for (int i = 0; i < listSize; i++) {
+		list.add(i);
+	}
+	for (int i = 0; i < listSize; i++) {
+		list.remove(0);
+	}
+
+	REQUIRE(list.size() == 0);
+}
+
+	SECTION( "Check bound indexes for remove" ){
+	int listSize = 200;
+	for (int i = 0; i < listSize; i++) {
+		list.add(i);
+	}
+	list.remove(0);
+	REQUIRE(list.size() == listSize - 1);
+
+	list.remove(list.size() - 1);
+	REQUIRE(list.size() == listSize - 2);
+
+	REQUIRE_THROWS_WITH(list.remove(-1), "Index -1 is out of range for 'remove' operation. Valid range: [0, 197]");
+	REQUIRE_THROWS_WITH(list.remove(list.size()), "Index 198 is out of range for 'remove' operation. Valid range: [0, 197]");
+	REQUIRE_THROWS_WITH(list.remove(1000), "Index 1000 is out of range for 'remove' operation. Valid range: [0, 197]");
+}
+
+	SECTION( "Check return value of remove method" ){
+	int listValue = 222888;
+	list.add(listValue);
+	REQUIRE(list.remove(0) == listValue);
+}
 }
 
