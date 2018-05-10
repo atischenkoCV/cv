58eb77a316a5ea7adf10ddfd844d5f2230b7df2d
atischenkoCV <atischenko.public@gmail.com>
2018-05-10 23:26:27 +0300

    [c++/collections] - add ArrayList#add tests

--- cpp/collections/CollectionsLibrary/src/ArrayListTest.cpp
+++ cpp/collections/CollectionsLibrary/src/ArrayListTest.cpp
@@ -4,9 +4,13 @@
 TEST_CASE( "ArrayList#iterator" , "[ArrayList]") {
 
 	SECTION("Common iterator test"){
-	int i[]
-	{	10, 20, 30};
-	ticher777::ArrayListIterator<int> iterator(i, 3);
+	ticher777::ArrayList<int> list(3);
+
+	list.add(10);
+	list.add(20);
+	list.add(30);
+
+	ticher777::ArrayListIterator<int> iterator = list.iterator();
 
 	REQUIRE(iterator.hasNext());
 	REQUIRE(iterator.next() == 10);
@@ -24,4 +28,40 @@ TEST_CASE( "ArrayList#iterator" , "[ArrayList]") {
 	REQUIRE(!iterator.hasNext());
 }
 
+}
+
+TEST_CASE( "ArrayList#add" , "[ArrayList]") {
+	ticher777::ArrayList<int> list(0);
+
+	SECTION("Check adding in empty list"){
+		list.add(0);
+
+		REQUIRE( list.size() == 1);
+	}
+
+		SECTION( "Check adding in full-filled list"){
+		for (int i = 0; i < MAX_LIST_SIZE; i++)
+		{
+			list.add(i);
+		}
+		REQUIRE( list.size() == MAX_LIST_SIZE);
+
+		REQUIRE_THROWS_WITH(list.add(-1), "List is full-filled.");
+	}
+		SECTION( "Check bound indexes for 'add' method" ){
+		REQUIRE_THROWS_WITH(list.add(10, -1), "Index 10 is out of range for 'add' operation. Valid range: [0, 0]");
+		list.add(11);
+
+		REQUIRE_THROWS_WITH(list.add(-1, 20), "Index 4294967295 is out of range for 'add' operation. Valid range: [0, 1]");
+	}
+
+		SECTION( "Check adding random values to list"){
+		srand (time(nullptr));
+
+		for (int i = 0; i < 200; i++)
+		{
+			list.add(rand());
+		}
+		REQUIRE(list.size() == 200);
+	}
 }
