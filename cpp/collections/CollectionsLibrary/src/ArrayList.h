b36efc7b317a15ab2224ac8e6d7590775103666b
atischenkoCV <atischenko.public@gmail.com>
2018-05-10 23:26:11 +0300

    [c++/collections] - implement base list methods for ArrayList

--- cpp/collections/CollectionsLibrary/src/ArrayList.h
+++ cpp/collections/CollectionsLibrary/src/ArrayList.h
@@ -9,12 +9,15 @@
 #define ARRAYLIST_H_
 
 #include "List.h"
+#include "iostream"
 
 namespace ticher777 {
 
 template<typename ArrayListIteratorType>
 class ArrayListIterator: public Iterator<ArrayListIteratorType> {
 public:
+	ArrayListIterator(const ArrayListIterator& iterator) = default;
+
 	ArrayListIterator(ArrayListIteratorType *array, int size) :
 			array(array), size(size) {
 	}
@@ -67,44 +70,65 @@ template<typename ArrayListType>
 class ArrayList: public List<ArrayListType, ArrayListIterator<ArrayListType>> {
 public:
 	ArrayList(const unsigned int initLenght) {
-		array = new ArrayListType[initLenght];
-		size = 0;
-		length = 0;
+		m_array = new ArrayListType[initLenght];
+		m_size = 0;
+		m_length = initLenght;
 	}
 
-	virtual ~ArrayList() = default;
-
-	unsigned int getLength() {
-		return 0;
+	virtual ~ArrayList() {
+		delete[] m_array;
 	}
 
-	virtual ArrayListIterator<ArrayListType>
-	iterator() const = 0;
+	unsigned int size() const override {
+		return m_size;
+	}
+
+	virtual ArrayListIterator<ArrayListType> iterator() const override {
+		ArrayListIterator<ArrayListType> result(m_array, m_size);
+		return result;
+	}
+	;
 protected:
 
 	virtual ArrayListType doGet(unsigned int index) const {
-		return 0;
+		return m_array[index];
 	}
-	;
 
 	virtual void doAdd(unsigned int index, ArrayListType value) {
-		//TODO[T7]: will be implement later
+		if (m_size == m_length) {
+			m_length = std::min<unsigned int>(m_length * 2 + 1, MAX_LIST_SIZE);
+			ArrayListType* newArray = new ArrayListType[m_length];
+			for (unsigned int i = 0; i < m_size; i ++) {
+				newArray[i] = m_array[i];
+			}
+			delete[] m_array;
+			m_array = newArray;
+		}
+
+		if (index != m_size) {
+			for (unsigned int i = m_size; i > index; i--) {
+				m_array[i] = m_array[i - 1];
+			}
+		}
+		m_array[index] = value;
+		m_size++;
 	}
-	;
 
 	/**
 	 * Safe remove method. Index is not out of the allowed range.
 	 */
 	virtual ArrayListType doRemove(unsigned int index) {
-		//TODO[T7]: will be implement later
-		return 0;
+		ArrayListType result = m_array[index];
+		for (unsigned int i = index; i < m_size - 1; i++) {
+			m_array[i] = m_array[i + 1];
+		}
+		return result;
 	}
-	;
 
 private:
-	ArrayListType *array;
-	unsigned int length;
-	unsigned int size;
+	ArrayListType *m_array;
+	unsigned int m_length;
+	unsigned int m_size;
 };
 
 } /* namespace ticher777 */
